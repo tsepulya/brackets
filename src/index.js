766c93688c5f99d0232960ebc1ed90cc0ad73b97
tsepulya <tsepeleva_anna@mail.ru>
2021-02-27 18:28:25 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let br = new Map(bracketsConfig)
+    let arr = [];
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === br.get(arr[arr.length - 1])) {
+            arr.pop();
+        } else {
+            arr.push(str[i])
+        }
+    }
+    return (arr.length === 0) ? true : false;
 }
